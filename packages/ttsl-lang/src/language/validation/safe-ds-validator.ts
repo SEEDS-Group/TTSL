0993c3ce6a0f7fe32fc1e85141c4666999ffdcf6
methr0 <s6tameth@uni-bonn.de>
2024-06-24 23:24:14 +0200

    remove unnecessary Validators

--- packages/ttsl-lang/src/language/validation/safe-ds-validator.ts
+++ packages/ttsl-lang/src/language/validation/safe-ds-validator.ts
@@ -2,18 +2,14 @@ import { ValidationChecks } from 'langium';
 import { TTSLAstType } from '../generated/ast.js';
 import type { SafeDsServices } from '../safe-ds-module.js';
 import {
-    annotationCallAnnotationShouldNotBeDeprecated,
     argumentCorrespondingParameterShouldNotBeDeprecated,
     assigneeAssignedResultShouldNotBeDeprecated,
-    namedTypeDeclarationShouldNotBeDeprecated,
     referenceTargetShouldNotBeDeprecated,
     requiredParameterMustNotBeDeprecated,
 } from './builtins/deprecated.js';
 import {
-    annotationCallAnnotationShouldNotBeExperimental,
     argumentCorrespondingParameterShouldNotBeExperimental,
     assigneeAssignedResultShouldNotBeExperimental,
-    namedTypeDeclarationShouldNotBeExperimental,
     referenceTargetShouldNotExperimental,
 } from './builtins/experimental.js';
 import { requiredParameterMustNotBeExpert } from './builtins/expert.js';
@@ -24,34 +20,16 @@ import {
     pythonNameShouldDifferFromSafeDsName,
 } from './builtins/pythonName.js';
 import { singleUseAnnotationsMustNotBeRepeated } from './builtins/repeatable.js';
-import { annotationCallMustHaveCorrectTarget, targetShouldNotHaveDuplicateEntries } from './builtins/target.js';
-import {
-    constraintListsShouldBeUsedWithCaution,
-    literalTypesShouldBeUsedWithCaution,
+import {    
     mapsShouldBeUsedWithCaution,
-    unionTypesShouldBeUsedWithCaution,
 } from './experimentalLanguageFeatures.js';
 import {
-    classMemberMustMatchOverriddenMemberAndShouldBeNeeded,
-    classMustNotInheritItself,
-    classMustOnlyInheritASingleClass,
-} from './inheritance.js';
-import {
-    annotationMustContainUniqueNames,
-    blockLambdaMustContainUniqueNames,
     callableTypeMustContainUniqueNames,
-    classMustContainUniqueNames,
-    enumMustContainUniqueNames,
-    enumVariantMustContainUniqueNames,
-    expressionLambdaMustContainUniqueNames,
     moduleMemberMustHaveNameThatIsUniqueInPackage,
     moduleMustContainUniqueNames,
     nameMustNotOccurOnCoreDeclaration,
     nameMustNotStartWithCodegenPrefix,
     nameShouldHaveCorrectCasing,
-    pipelineMustContainUniqueNames,
-    schemaMustContainUniqueNames,
-    segmentMustContainUniqueNames,
 } from './names.js';
 import {
     argumentListMustNotHavePositionalArgumentsAfterNamedArguments,
@@ -60,11 +38,8 @@ import {
     argumentListMustSetAllRequiredParameters,
 } from './other/argumentLists.js';
 import {
-    annotationCallArgumentsMustBeConstant,
-    annotationCallMustNotLackArgumentList,
     callableTypeParametersMustNotBeAnnotated,
     callableTypeResultsMustNotBeAnnotated,
-    lambdaParametersMustNotBeAnnotated,
 } from './other/declarations/annotationCalls.js';
 import { parameterListMustNotHaveRequiredParametersAfterOptionalParameters } from './other/declarations/parameterLists.js';
 import {
@@ -72,23 +47,8 @@ import {
     constantParameterMustHaveTypeThatCanBeEvaluatedToConstant,
 } from './other/declarations/parameters.js';
 import { placeholderShouldBeUsed, placeholdersMustNotBeAnAlias } from './other/declarations/placeholders.js';
-import {
-    segmentParameterShouldBeUsed,
-    segmentResultMustBeAssignedExactlyOnce,
-    segmentShouldBeUsed,
-} from './other/declarations/segments.js';
-import {
-    typeParameterMustBeUsedInCorrectPosition,
-    typeParameterMustHaveSufficientContext,
-    typeParameterMustOnlyBeVariantOnClass,
-    typeParameterUpperBoundMustBeNamedType,
-} from './other/declarations/typeParameters.js';
 import { callArgumentMustBeConstantIfParameterIsConstant, callMustNotBeRecursive } from './other/expressions/calls.js';
 import { divisionDivisorMustNotBeZero } from './other/expressions/infixOperations.js';
-import {
-    lambdaMustBeAssignedToTypedParameter,
-    lambdaParameterMustNotHaveConstModifier,
-} from './other/expressions/lambdas.js';
 import { memberAccessOfEnumVariantMustNotLackInstantiation } from './other/expressions/memberAccesses.js';
 import {
     referenceMustNotBeFunctionPointer,
@@ -105,29 +65,12 @@ import {
 import {
     assignmentAssigneeMustGetValue,
     assignmentShouldNotImplicitlyIgnoreResult,
-    yieldMustNotBeUsedInPipeline,
 } from './other/statements/assignments.js';
 import {
     callableTypeMustBeUsedInCorrectContext,
     callableTypeMustNotHaveOptionalParameters,
     callableTypeParameterMustNotHaveConstModifier,
 } from './other/types/callableTypes.js';
-import {
-    literalTypeMustHaveLiterals,
-    literalTypeMustNotContainListLiteral,
-    literalTypeMustNotContainMapLiteral,
-    literalTypeShouldNotHaveDuplicateLiteral,
-} from './other/types/literalTypes.js';
-import {
-    namedTypeMustNotHaveTooManyTypeArguments,
-    namedTypeMustNotSetTypeParameterMultipleTimes,
-    namedTypeTypeArgumentListMustNotHavePositionalArgumentsAfterNamedArguments,
-} from './other/types/namedTypes.js';
-import {
-    unionTypeMustBeUsedInCorrectContext,
-    unionTypeMustHaveTypes,
-    unionTypeShouldNotHaveDuplicateTypes,
-} from './other/types/unionTypes.js';
 import {
     callArgumentAssignedToPureParameterMustBePure,
     impurityReasonParameterNameMustBelongToParameterOfCorrectType,
@@ -136,26 +79,14 @@ import {
     pureParameterDefaultValueMustBePure,
 } from './purity.js';
 import {
-    annotationCallArgumentListShouldBeNeeded,
-    annotationParameterListShouldNotBeEmpty,
-    annotationParameterShouldNotHaveConstModifier,
     assignmentShouldHaveMoreThanWildcardsAsAssignees,
     callArgumentListShouldBeNeeded,
     chainedExpressionNullSafetyShouldBeNeeded,
-    classBodyShouldNotBeEmpty,
-    constraintListShouldNotBeEmpty,
     elvisOperatorShouldBeNeeded,
-    enumBodyShouldNotBeEmpty,
-    enumVariantParameterListShouldNotBeEmpty,
     functionResultListShouldNotBeEmpty,
     importedDeclarationAliasShouldDifferFromDeclarationName,
-    namedTypeTypeArgumentListShouldBeNeeded,
-    segmentResultListShouldNotBeEmpty,
-    typeParameterListShouldNotBeEmpty,
-    unionTypeShouldNotHaveASingularTypeArgument,
 } from './style.js';
 import {
-    attributeMustHaveTypeHint,
     callArgumentTypesMustMatchParameterTypes,
     callReceiverMustBeCallable,
     indexedAccessIndexMustHaveCorrectType,
@@ -163,27 +94,20 @@ import {
     infixOperationOperandsMustHaveCorrectType,
     listMustNotContainNamedTuples,
     mapMustNotContainNamedTuples,
-    namedTypeMustSetAllTypeParameters,
-    namedTypeTypeArgumentsMustMatchBounds,
     parameterDefaultValueTypeMustMatchParameterType,
     parameterMustHaveTypeHint,
     prefixOperationOperandMustHaveCorrectType,
     resultMustHaveTypeHint,
     typeCastExpressionMustHaveUnknownType,
-    typeParameterDefaultValueMustMatchUpperBound,
-    yieldTypeMustMatchResultType,
 } from './types.js';
 import { statementMustDoSomething } from './other/statements/statements.js';
 import { indexedAccessIndexMustBeValid } from './other/expressions/indexedAccess.js';
-import { typeParameterListMustNotHaveRequiredTypeParametersAfterOptionalTypeParameters } from './other/declarations/typeParameterLists.js';
 import { chainedExpressionsMustBeNullSafeIfReceiverIsNullable } from './other/expressions/chainedExpressions.js';
 import {
     callArgumentMustRespectParameterBounds,
-    parameterBoundParameterMustBeConstFloatOrInt,
-    parameterBoundRightOperandMustEvaluateToFloatConstantOrIntConstant,
     parameterDefaultValueMustRespectParameterBounds,
 } from './other/declarations/parameterBounds.js';
-import { groupByVariableMustBeAnID, groupedFunctionHasAggregation, groupedFunctionHasValidID } from './aggregation.js';
+import { groupedFunctionHasAggregation, groupedFunctionHasValidID } from './aggregation.js';
 
 /**
  * Register custom validation checks.
@@ -204,20 +128,6 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             argumentListMustNotHaveTooManyArguments(services),
             argumentListMustSetAllRequiredParameters(services),
         ],
-        TslAnnotation: [
-            annotationMustContainUniqueNames,
-            annotationParameterListShouldNotBeEmpty(services),
-            annotationParameterShouldNotHaveConstModifier(services),
-            targetShouldNotHaveDuplicateEntries(services),
-        ],
-        TslAnnotationCall: [
-            annotationCallAnnotationShouldNotBeDeprecated(services),
-            annotationCallAnnotationShouldNotBeExperimental(services),
-            annotationCallArgumentListShouldBeNeeded(services),
-            annotationCallArgumentsMustBeConstant(services),
-            annotationCallMustHaveCorrectTarget(services),
-            annotationCallMustNotLackArgumentList,
-        ],
         TslArgument: [
             argumentCorrespondingParameterShouldNotBeDeprecated(services),
             argumentCorrespondingParameterShouldNotBeExperimental(services),
@@ -226,11 +136,6 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             argumentListMustNotHavePositionalArgumentsAfterNamedArguments,
             argumentListMustNotSetParameterMultipleTimes(services),
         ],
-        TslAttribute: [attributeMustHaveTypeHint],
-        TslAggregation: [
-            groupByVariableMustBeAnID(),
-        ],
-        TslBlockLambda: [blockLambdaMustContainUniqueNames],
         TslCall: [
             callArgumentListShouldBeNeeded(services),
             callArgumentAssignedToPureParameterMustBePure(services),
@@ -252,14 +157,6 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             chainedExpressionsMustBeNullSafeIfReceiverIsNullable(services),
             chainedExpressionNullSafetyShouldBeNeeded(services),
         ],
-        TslClass: [
-            classMustContainUniqueNames,
-            classMustOnlyInheritASingleClass(services),
-            classMustNotInheritItself(services),
-        ],
-        TslClassBody: [classBodyShouldNotBeEmpty(services)],
-        TslClassMember: [classMemberMustMatchOverriddenMemberAndShouldBeNeeded(services)],
-        TslConstraintList: [constraintListsShouldBeUsedWithCaution(services), constraintListShouldNotBeEmpty(services)],
         TslDeclaration: [
             nameMustNotOccurOnCoreDeclaration(services),
             nameMustNotStartWithCodegenPrefix,
@@ -267,10 +164,6 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             pythonNameShouldDifferFromSafeDsName(services),
             singleUseAnnotationsMustNotBeRepeated(services),
         ],
-        TslEnum: [enumMustContainUniqueNames],
-        TslEnumBody: [enumBodyShouldNotBeEmpty(services)],
-        TslEnumVariant: [enumVariantMustContainUniqueNames, enumVariantParameterListShouldNotBeEmpty(services)],
-        TslExpressionLambda: [expressionLambdaMustContainUniqueNames],
         TslFunction: [
             functionResultListShouldNotBeEmpty(services),
             impurityReasonsOfOverridingMethodMustBeSubsetOfOverriddenMethod(services),
@@ -293,19 +186,7 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             elvisOperatorShouldBeNeeded(services),
             infixOperationOperandsMustHaveCorrectType(services),
         ],
-        TslLambda: [
-            lambdaMustBeAssignedToTypedParameter(services),
-            lambdaParametersMustNotBeAnnotated,
-            lambdaParameterMustNotHaveConstModifier,
-        ],
         TslList: [listMustNotContainNamedTuples(services)],
-        TslLiteralType: [
-            literalTypeMustHaveLiterals,
-            literalTypeMustNotContainListLiteral,
-            literalTypeMustNotContainMapLiteral,
-            literalTypesShouldBeUsedWithCaution(services),
-            literalTypeShouldNotHaveDuplicateLiteral(services),
-        ],
         TslDictionary: [mapMustNotContainNamedTuples(services), mapsShouldBeUsedWithCaution(services)],
         TslMemberAccess: [memberAccessOfEnumVariantMustNotLackInstantiation],
         TslModule: [
@@ -315,16 +196,6 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             moduleWithDeclarationsMustStatePackage,
             pipelineFileMustNotBeInBuiltinPackage,
             pythonModuleShouldDifferFromSafeDsPackage(services),
-        ],
-        TslNamedType: [
-            namedTypeDeclarationShouldNotBeDeprecated(services),
-            namedTypeDeclarationShouldNotBeExperimental(services),
-            namedTypeMustNotHaveTooManyTypeArguments,
-            namedTypeMustNotSetTypeParameterMultipleTimes(services),
-            namedTypeMustSetAllTypeParameters(services),
-            namedTypeTypeArgumentListShouldBeNeeded(services),
-            namedTypeTypeArgumentListMustNotHavePositionalArgumentsAfterNamedArguments,
-            namedTypeTypeArgumentsMustMatchBounds(services),
         ],
         TslParameter: [
             constantParameterMustHaveConstantDefaultValue(services),
@@ -336,12 +207,7 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             requiredParameterMustNotBeDeprecated(services),
             requiredParameterMustNotBeExpert(services),
         ],
-        TslParameterBound: [
-            parameterBoundParameterMustBeConstFloatOrInt(services),
-            parameterBoundRightOperandMustEvaluateToFloatConstantOrIntConstant(services),
-        ],
         TslParameterList: [parameterListMustNotHaveRequiredParametersAfterOptionalParameters],
-        TslPipeline: [pipelineMustContainUniqueNames],
         TslPlaceholder: [placeholdersMustNotBeAnAlias, placeholderShouldBeUsed(services)],
         TslPrefixOperation: [prefixOperationOperandMustHaveCorrectType(services)],
         TslReference: [
@@ -352,36 +218,9 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             referenceTargetShouldNotExperimental(services),
         ],
         TslResult: [resultMustHaveTypeHint],
-        TslSchema: [schemaMustContainUniqueNames],
-        TslSegment: [
-            segmentMustContainUniqueNames,
-            segmentParameterShouldBeUsed(services),
-            segmentResultMustBeAssignedExactlyOnce(services),
-            segmentResultListShouldNotBeEmpty(services),
-            segmentShouldBeUsed(services),
-        ],
         TslStatement: [statementMustDoSomething(services)],
         TslTemplateString: [templateStringMustHaveExpressionBetweenTwoStringParts],
         TslTypeCast: [typeCastExpressionMustHaveUnknownType(services)],
-        TslTypeParameter: [
-            typeParameterDefaultValueMustMatchUpperBound(services),
-            typeParameterMustBeUsedInCorrectPosition(services),
-            typeParameterMustHaveSufficientContext,
-            typeParameterMustOnlyBeVariantOnClass,
-            typeParameterUpperBoundMustBeNamedType(services),
-        ],
-        TslTypeParameterList: [
-            typeParameterListMustNotHaveRequiredTypeParametersAfterOptionalTypeParameters,
-            typeParameterListShouldNotBeEmpty(services),
-        ],
-        TslUnionType: [
-            unionTypeMustBeUsedInCorrectContext,
-            unionTypeMustHaveTypes,
-            unionTypesShouldBeUsedWithCaution(services),
-            unionTypeShouldNotHaveDuplicateTypes(services),
-            unionTypeShouldNotHaveASingularTypeArgument(services),
-        ],
-        TslYield: [yieldMustNotBeUsedInPipeline, yieldTypeMustMatchResultType(services)],
     };
     registry.register(checks);
 };
