8f3078251bcc19dc3d3e0293c38fc94324aa5d4b
methr0 <s6tameth@uni-bonn.de>
2024-11-26 12:25:54 +0100

    fix and test simulation command

--- packages/ttsl-cli/src/cli/simulate.ts
+++ packages/ttsl-cli/src/cli/simulate.ts
@@ -1,9 +1,11 @@
 import { createTTSLServices } from '@ttsl/lang';
+import { URI } from 'langium';
 import { NodeFileSystem } from 'langium/node';
 
-export const simulate = async (date: string, data: string, targets: string, fsPaths: string[], ): Promise<void> => {
+export const simulate = async (date: string, data: string, targets: string, fsPaths: string ): Promise<void> => {
     const services = (await createTTSLServices(NodeFileSystem)).TTSL;
-    
+
+    let uri = URI.file(fsPaths)
     const runner = services.runtime.Runner
-    runner.runFunction("./helpers/simulate.ttsl", "simulate", [date, data, targets])
+    runner.runSimulation(uri.toString(), "simulate", [date, data, targets])
 }
